282b0f7cf52c923077e6d7162d54cc7f19079504
buchihiro <buchihiro5147gaming@gmail.com>
2023-09-02 23:55:32 +0900

    chakra ui をインストール

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,32 @@
+import {
+  Tabs,
+  TabList,
+  TabPanels,
+  Tab,
+  TabPanel,
+  ChakraProvider,
+} from "@chakra-ui/react";
+
 export default function Home() {
   return (
     <>
-      <h1>攻略サイト</h1>
+      <ChakraProvider>
+        <h1>攻略サイト</h1>
+        <Tabs variant="soft-rounded" colorScheme="green">
+          <TabList>
+            <Tab>Tab 1</Tab>
+            <Tab>Tab 2</Tab>
+          </TabList>
+          <TabPanels>
+            <TabPanel>
+              <p>one!</p>
+            </TabPanel>
+            <TabPanel>
+              <p>two!</p>
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
+      </ChakraProvider>
     </>
-  )
+  );
 }
