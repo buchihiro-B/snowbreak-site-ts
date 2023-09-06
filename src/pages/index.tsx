077799ec43df24592917baed650196f8d193f456
buchihiro <buchihiro5147gaming@gmail.com>
2023-09-06 19:32:57 +0900

    finish part2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,53 @@ import {
   TabPanel,
   ChakraProvider,
 } from "@chakra-ui/react";
+import Head from "next/head";
+import Link from "next/link";
+import Layout, { siteTitle } from "../../components/layout";
+import utilStyles from "../../styles/utils.module.css";
+import { getSortedPostsData } from "../../lib/posts";
 
-export default function Home() {
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }: { allPostsData: Array<any> }) {
   return (
-    <>
+    <Layout isHome>
       <ChakraProvider>
-        <h1>攻略サイト</h1>
+        <Head>
+          <title>{siteTitle}</title>
+        </Head>
+
+        <section className={utilStyles.headingMd}>
+          <p>[Your Self Introduction]</p>
+          <p>
+            (This is a sample website - you’ll be building a site like this on{" "}
+            <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          </p>
+        </section>
+
+        <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+          <h2 className={utilStyles.headingLg}>Blog</h2>
+          <ul className={utilStyles.list}>
+            {allPostsData.map(({ id, date, title }) => (
+              <li className={utilStyles.listItem} key={id}>
+                {title}
+                <br />
+                {id}
+                <br />
+                {date}
+              </li>
+            ))}
+          </ul>
+        </section>
+
+        {/* <h1>攻略サイト</h1>
         <Tabs variant="soft-rounded" colorScheme="green">
           <TabList>
             <Tab>Tab 1</Tab>
@@ -25,8 +66,8 @@ export default function Home() {
               <p>two!</p>
             </TabPanel>
           </TabPanels>
-        </Tabs>
+        </Tabs> */}
       </ChakraProvider>
-    </>
+    </Layout>
   );
 }
